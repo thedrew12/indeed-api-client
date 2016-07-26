a2f1d0387883e77c5fa3c14d342f97a57cc1f70f
Andrew Sumpter <andrew.sumpter@economicmodeling.com>
2016-07-26 15:54:29 -0700

    moved the indeed contrucstor to the index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-export { default as Indeed } from './src/Indeed';
+import jobSearch from './src/jobSearch';
+
+export default function Indeed(url = 'http://api.indeed.com/ads', publisherId) {
+	if (!publisherId) {
+		throw Error('An Indeed publisher id is required');
+	} else {
+		return {
+			jobSearch: () => jobSearch(url, publisherId)
+		};
+	}
+}
--- src/Indeed.js
+++ src/Indeed.js
@@ -1,11 +1,1 @@
-import jobSearch from './JobSearch';
 
-export default function Indeed(url = 'http://api.indeed.com/ads', publisherId) {
-	if (!publisherId) {
-		throw Error('An Indeed publisher id is required');
-	} else {
-		return {
-			jobSearch: () => jobSearch(url, publisherId)
-		};
-	}
-}
