69ae24e0f867d9c8f28152f0e031f39f75f965d6
Andrew Sumpter <andrew.sumpter@economicmodeling.com>
2016-07-27 14:05:06 -0700

    changed the main prop in package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import jobSearch from './JobSearch';
 
 export default function Indeed(url = 'http://api.indeed.com/ads', publisherId) {
-  console.log('here');
 	if (!publisherId) {
 		throw Error('An Indeed publisher id is required');
 	} else {
