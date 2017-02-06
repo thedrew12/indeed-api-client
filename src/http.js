12c052fcc3fbfaff6fe1b74f8bd1f3a71db3dfdc
Andrew Sumpter <andrew.sumpter@economicmodeling.com>
2017-02-06 15:36:11 -0800

    dropped the proxy object and went with a more basic approach

--- src/http.js
+++ src/http.js
@@ -7,6 +7,5 @@ import fetch from 'isomorphic-fetch';
  * @param {array} params The Indeed API params
  */
 export function getData(url) {
-	return fetch(url)
-		.then(response => response.json());
+	return fetch(url).then(response => response.json());
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import jobSearch from './jobSearch';
-require('harmony-reflect');
 
 export default function Indeed(url = 'http://api.indeed.com/ads', publisherId) {
 	if (!publisherId) {
